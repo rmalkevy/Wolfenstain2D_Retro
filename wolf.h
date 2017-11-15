e19993c4da8fd1dfb63306a3f9f3bba35540eb1b
Roman Malkevych <Henprokuror@MacBook-Pro-Roman.local>
2017-11-15 14:11:28 +0200

    First commit

---
+++ wolf.h
@@ -0,0 +1,17 @@
+//
+// Created by Roman Malkevych on 10/28/17.
+//
+
+#ifndef WOLF_WOLF_H
+#define WOLF_WOLF_H
+
+typedef struct	s_win
+{
+	void		    *mlx;
+	void		    *win;
+	void		    *image;
+	char		    *line;
+	unsigned int    color;
+}				t_win;
+
+#endif //WOLF_WOLF_H
